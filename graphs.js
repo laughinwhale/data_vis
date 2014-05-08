96c8d096acae156310429571cf91d44087423311
laughinwhale <hinwha777@gmail.com>
2014-05-08 12:17:37 -0500

    Added JS with the first example graph!

---
+++ graphs.js
@@ -0,0 +1,26 @@
+var graph1; // globally available
+$(function () { 
+				$('#graph1').highcharts({
+					chart: {
+						type: 'bar'
+					},
+					title: {
+						text: 'Fruit Consumption'
+					},
+					xAxis: {
+						categories: ['Apples', 'Bananas', 'Oranges']
+					},
+					yAxis: {
+						title: {
+							text: 'Fruit eaten'
+						}
+					},
+					series: [{
+						name: 'Jane',
+						data: [1, 0, 4]
+					}, {
+						name: 'John',
+						data: [5, 7, 3]
+					}]
+				});
+			});
